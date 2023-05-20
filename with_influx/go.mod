b3b668ec834946cdda06d6b21694f642ba7b3811
studnet-ops <gogakuec@gmail.com>
2023-05-20 04:15:58 +0000

    succes db connection

--- with_influx/go.mod
+++ with_influx/go.mod
@@ -1,6 +1,6 @@
 module test
 
-go 1.20
+go 1.17
 
 require github.com/influxdata/influxdb-client-go/v2 v2.12.3
 
--- with_influx/src/main.go
+++ with_influx/src/main.go
@@ -2,30 +2,31 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"sort"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
-type SurroundingsPalyload struct {
+type SurroundingsPayload struct {
 	Number      int       `json:"number"`
 	Timestamp   time.Time `json:"timestamp"`
-	Tempreture  float64   `json:"tempreture"`
-	Moisuture   float64   `josn:"moisuture"`
+	Temperature float64   `json:"Temperature"`
+	Moisture    float64   `josn:"Moisture"`
 	AirPressure float64   `json:"airPressure"`
 }
 
-func insertPayload(payload []SurroundingsPalyload) {
+func insertPayload(payload []SurroundingsPayload) {
 	var token string
 	var bucket string
 	var org string
 	var dbUrl string
-	token = os.Getenv("INFLUXDB_TOKEN")
-	bucket = os.Getenv("INFLUXDB_BUCKET")
-	org = os.Getenv("INFLUXDB_ORG")
-	dbUrl = os.Getenv("DB_URL")
+
+	token = "F-QFQpmCL9UkR3qyoXnLkzWj03s6m4eCvYgDl1ePfHBf9ph7yxaSgQ6WN0i9giNg"
+	bucket = "vuoy_monitor"
+	org = "iot"
+	dbUrl = "http://influxdb:8086"
+
 	fmt.Printf("connectingt to %s , bucket :%s ,org :%s ,token :%s\n", dbUrl, bucket, org, token)
 	client := influxdb2.NewClient(dbUrl, token)
 	writeAPI := client.WriteAPI(org, bucket)
@@ -40,17 +41,17 @@ func insertPayload(payload []SurroundingsPalyload) {
 		return payload[i].Number < payload[j].Number
 	})
 
-	surroundings := make([]SurroundingsPalyload, len(payload))
+	surroundings := make([]SurroundingsPayload, len(payload))
 	for i, v := range payload {
 		surroundings[i] = v
 	}
 
 	for i, v := range surroundings {
-		fmt.Println(fmt.Printf("%d: number: %d, timestamp: %s, tempreture: %f, moisuture: %f, airPressure: %f", i, v.Number, v.Timestamp, v.Tempreture, v.Moisuture, v.AirPressure))
+		fmt.Println(fmt.Printf("%d: number: %d, timestamp: %s, Temperature: %f, Moisture: %f, airPressure: %f", i, v.Number, v.Timestamp, v.Temperature, v.Moisture, v.AirPressure))
 		p := influxdb2.NewPointWithMeasurement("vuoy_surroundings").
 			AddTag("user", "bar").
-			AddField("Tempreture", v.Tempreture).
-			AddField("Moisuture", v.Moisuture).
+			AddField("Temperature", v.Temperature).
+			AddField("Moisture", v.Moisture).
 			AddField("AirPressure", v.AirPressure).
 			SetTime(time.Now())
 		fmt.Printf("time: %s\n", v.Timestamp)
@@ -61,6 +62,30 @@ func insertPayload(payload []SurroundingsPalyload) {
 	return
 }
 
-func main(){
+func main() {
+	payload := []SurroundingsPayload{
+		{
+			Number:      1,
+			Timestamp:   time.Now(),
+			Temperature: 25.5,
+			Moisture:    0.8,
+			AirPressure: 1013.25,
+		},
+		{
+			Number:      2,
+			Timestamp:   time.Now(),
+			Temperature: 26.1,
+			Moisture:    0.9,
+			AirPressure: 1012.75,
+		},
+		{
+			Number:      3,
+			Timestamp:   time.Now(),
+			Temperature: 25.8,
+			Moisture:    0.7,
+			AirPressure: 1013.50,
+		},
+	}
 
+	insertPayload(payload)
 }
